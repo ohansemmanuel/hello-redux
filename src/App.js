6293b5a7778e833d348346ca16aa1124b99ca50e
ohansemmanuel <ohansemmanuel@outlook.com>
2020-10-01 07:15:14 +0200

    complete solution

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import HelloWorld from "./HelloWorld";
 
+//import reducer
+import reducer from "./reducers";
+import { createStore } from "redux";
+const initialState = "React";
+const store = createStore(reducer, initialState);
+
 const App = () => {
-  const [tech] = useState("React");
-  return <HelloWorld tech={tech} />;
+  return <HelloWorld tech={store.getState()} />;
 };
 
 export default App;
