91ef2dc67d593d7ec565d97522a6afba32ce7b71
ohansemmanuel <ohansemmanuel@outlook.com>
2020-10-01 07:36:42 +0200

    cleaup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import HelloWorld from "./HelloWorld";
-
 //import reducer
 import reducer from "./reducers";
 import { createStore } from "redux";
+
 const initialState = "React";
 const store = createStore(reducer, initialState);
 
